1454dfb03c36ccb2637a760b5efeda7b3c146113
hani <hanisoft2022@gmail.com>
2025-01-31 00:00:55 +0900

    riverpod provider scope start

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vid_player/s_home.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    const ProviderScope(
+        child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SHome(),
-    ),
+    )),
   );
 }
