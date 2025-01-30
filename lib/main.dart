cdb811371ef7a0a21d98f62fd3c6f9fb797090a8
hani <hanisoft2022@gmail.com>
2025-01-31 02:46:40 +0900

    just provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vid_player/s_home.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    const ProviderScope(
+        child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SHome(),
-    ),
+    )),
   );
 }
 
